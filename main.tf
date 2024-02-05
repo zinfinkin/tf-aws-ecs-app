04298cbc5580766f2d59fe9ffb815738e15b00ed
Mitchell Lyon <mitchell_lyon@yahoo.com>
2024-02-05 17:33:22 -0600

    NEW TF commit

--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
-# Provider block specifying Terraform Cloud as our remote backend. 
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
   cloud {
-    organization = "athome"
+    organization = "Mitchlyon"
+
     workspaces {
-      name = "tf-aws-demo"
+      name = "tf-aws-ecs-app"
     }
   }
 }
-
-
-
-
-
-
-
-
-
